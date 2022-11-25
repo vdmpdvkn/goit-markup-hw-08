8e16f2136367b5cdc1da3b5434c4eb08a3eee1c2
Vadim Pudovkin <traplordhoncho@gmail.com>
2022-11-25 21:45:24 +0300

    сделал адаптивную модалку и начал делать меню

--- js/menu.js
+++ js/menu.js
@@ -7,6 +7,7 @@
       menuBtnRef.getAttribute("aria-expanded") === "true" || false;
 
     menuBtnRef.classList.toggle("is-open");
+    document.body.classList.toggle("modal-open");
     menuBtnRef.setAttribute("aria-expanded", !expanded);
 
     mobileMenuRef.classList.toggle("is-open");
