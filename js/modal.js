48ecd6a1e8a6652d2c9efb5a7b583ab415efe69f
Vadim Pudovkin <traplordhoncho@gmail.com>
2022-11-28 17:25:04 +0300

    доделал модальное окно и мобильное меню

--- js/modal.js
+++ js/modal.js
@@ -1,31 +1,15 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
-    document.body.classList.toggle('modal-open');
-    refs.modal.classList.toggle('is-hidden');
+    document.body.classList.toggle("modal-open");
+    refs.modal.classList.toggle("is-hidden");
   }
-})();
-(() => {
-  document.querySelector('.js-speaker-form').addEventListener('submit', e => {
-    e.preventDefault();
-
-    new FormData(e.currentTarget).forEach((value, name) => console.log(`${name}: ${value}`));
-    e.currentTarget.reset();
-  });
-})();
-(() => {
-  document.querySelector('.js-form').addEventListener('submit', e => {
-    e.preventDefault();
-
-    new FormData(e.currentTarget).forEach((value, name) => console.log(`${name}: ${value}`));
-    e.currentTarget.reset();
-  });
 })();
---
+++ js/submit.js
@@ -0,0 +1,20 @@
+(() => {
+  document.querySelector(".js-speaker-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    new FormData(e.currentTarget).forEach((value, name) =>
+      console.log(`${name}: ${value}`)
+    );
+    e.currentTarget.reset();
+  });
+})();
+(() => {
+  document.querySelector(".js-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    new FormData(e.currentTarget).forEach((value, name) =>
+      console.log(`${name}: ${value}`)
+    );
+    e.currentTarget.reset();
+  });
+})();
